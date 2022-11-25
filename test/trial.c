4941da3212cfbe8f5d5b30f9b7215ac00b2c1136
심재민 <104059642+simjaemin1@users.noreply.github.com>
2022-11-25 14:29:45 +0900

    Update trial.c

--- test/trial.c
+++ test/trial.c
@@ -8,46 +8,45 @@
 
 
 void prime_factor(int num) {
-	printf("%d =  ", num);
-	int i =2;
-	while(i<num) {
-		if(num%i)
+	printf("%d = ", num);
+	int i = 2;
+	while (i < num) {
+		if (num % i)
 			i++;
 		else {
-			num/=i;
-			printf("%d*", i);
+			num /= i;
+			printf("%d * ", i);
 		}
 	}
 	printf("%d\n", num);
 }
 
-
-int main(int argc, char *argv[]){
-	int num;
-	int identifier;
-    FILE *fp;
-    num = 0;
-	if(argc!=2)
-	{
-		printf("error: use only one argument\n");
-		return 0;
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		printf("Type \"/root/selector [process_num]\"\n");
+		return -1;
 	}
-	identifier=atoi(argv[1]);
-	while(1)
-	{
-		syscall(ROTLOCK_READ, 90, 90);
-		fp = fopen("integer", "r");
-		if(fp==0)
-		{
-			printf("error: no such file integer\n");
-			return 0;
+	int num;
+	int process_num = atoi(argv[1]);
+    FILE *fp;
+
+	while (1) {
+		if (syscall(ROTLOCK_READ, 90, 90) < 0)
+			return -1;
+		while (1) {
+			syscall(ROTUNLOCK_READ, 90, 90);
+			if (fp = fopen("integer", "r")) {
+				syscall(ROTLOCK_READ, 90, 90);
+				break;
+			}
 		}
 		fscanf(fp, "%d", &num);
-		printf("trial-%d : ", identifier);
+		printf("trial-%d: ", process_num);
 		prime_factor(num);
 		fclose(fp);
-		syscall(ROTUNLOCK_READ, 90, 90);
+		if (syscall(ROTUNLOCK_READ, 90, 90) < 0)
+			return -1;
         sleep(1);
 	}
+	return 0;
 }
-

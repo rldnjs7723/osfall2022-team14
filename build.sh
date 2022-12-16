9518fb25e948be5f471173c8271a44ce8f697b0a
rldnjs7723 <rldnjs7723@naver.com>
2022-12-17 06:25:48 +0900

    Auto Extract e2fsprogs

--- build.sh
+++ build.sh
@@ -11,8 +11,12 @@ rm -f ../tizen-image/boot.img ../tizen-image/modules.img ../tizen-image/ramdisk.
 echo "-----------Move Image File-----------"
 mv boot.img modules.img ../tizen-image
 tar xvzf tizen-unified_20181024.1_iot-headless-2parts-armv7l-rpi3.tar.gz -C ../tizen-image
-sudo mount ../tizen-image/rootfs.img ../tizen-image/mnt_dir
+echo "-----------Extract e2fsprogs-----------"
+rm -r -f ./e2fsprogs
+tar xvzf e2fsprogs-1.46.5.tar.gz -C ./
+mv e2fsprogs-1.46.5 e2fsprogs
 echo "-----------Compile & Move Test File-----------"
+sudo mount ../tizen-image/rootfs.img ../tizen-image/mnt_dir
 cd ./test
 make
 sudo cp gpsupdate file_loc file_write ../../tizen-image/mnt_dir/root

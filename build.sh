30c19672e7bcf52893ebe2a53a9b21ff95c2fdf6
simjaemin1 <simjaemin1@snu.ac.kr>
2022-12-17 03:53:26 +0900

    edit gps.c

--- build.sh
+++ build.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-echo "-----------Make Directory-----------"
-mkdir -p ../tizen-image/mnt_dir
-echo "-----------Build-----------"
 sudo ./build-rpi3-arm64.sh
-echo "-----------Make Image File-----------"
-sudo ./scripts/mkbootimg_rpi3.sh
-echo "-----------Remove Prev Image File-----------"
-rm -f ../tizen-image/boot.img ../tizen-image/modules.img ../tizen-image/ramdisk.img ../tizen-image/ramdisk-recovery.img ../tizen-image/rootfs.img ../tizen-image/system-data.img
-echo "-----------Move Image File-----------"
-mv boot.img modules.img ../tizen-image
-tar xvzf tizen-unified_20181024.1_iot-headless-2parts-armv7l-rpi3.tar.gz -C ../tizen-image
-echo "-----------Compile Test File-----------"
-arm-linux-gnueabi-gcc -I/include test/rotd.c -o test/rotd
-arm-linux-gnueabi-gcc -I/include test/selector.c -o test/selector
-arm-linux-gnueabi-gcc -I/include test/trial.c -o test/trial
-echo "-----------Move Test File-----------"
-sudo mount ../tizen-image/rootfs.img ../tizen-image/mnt_dir
-sudo cp ./test/rotd ./test/selector ./test/trial ../tizen-image/mnt_dir/root
-echo "-----------Change fstab Permission-----------"
-sudo cp ./fstab ../tizen-image/mnt_dir/etc/
-echo "-----------Wait Unmount-----------"
-sleep 5
-sudo umount ../tizen-image/mnt_dir
-echo "-----------Done-----------"
+cd ../tizen-image
+sudo mount rootfs.img ../mnt_dir
+cd ../osfall2022-team14
+cd test
+make
+sudo cp gpsupdate ../../mnt_dir/root;
+sudo cp file_loc ../../mnt_dir/root;
+sudo cp file_write ../../mnt_dir/root;
+cd ../e2fsprogs
+./configure
+make
+cd ../
+dd if=/dev/zero of=proj4.fs bs=1M count=1
+sudo losetup /dev/loop14 proj4.fs
+sudo ./e2fsprogs/misc/mke2fs -I 256 -L os.proj4 /dev/loop14
+sudo losetup -d /dev/loop14
+sudo mv proj4.fs ../mnt_dir/root/
+sudo umount ../mnt_dir
+sudo ./qemu.sh
